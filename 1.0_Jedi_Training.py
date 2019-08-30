41f15615b2840a334e3996d3755ea71ffe231e8b
leavertonnel <leavertonnel@urbandaleschools.com>
2019-08-30 13:54:22 -0500

    merging into the master
    # Conflicts:
    #	1.1_Github_VCS.py

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -6,9 +6,9 @@ Define Forking: copying someone else's repository into your own
 Define Cloning: is used to get an existing repository and create a cline
 in your local or remote repository
 
-Define Branching:
+Define Branching: when you make a copy but in the same place as the master
 
-Define Committing: 
+Define Committing: you make a save but you also make a comment when you save.
 
 Define Merging: lets you put your forking history back together
 
