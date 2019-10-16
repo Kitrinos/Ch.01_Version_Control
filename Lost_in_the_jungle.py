07a30df178319346f268c49a21b71501aa8a36d8
Nellie Leaverton <54453366+Kitrinos@users.noreply.github.com>
2019-10-16 10:41:51 -0500

    Create Lost_in_the_jungle.py

---
+++ Lost_in_the_jungle.py
@@ -0,0 +1,10 @@
+#Mortal Misadventure
+#Ch_1 : Lost in the Jungle
+
+string = " Mortal Misadventure"
+
+new_string = string.center(130)
+
+print(new_string)
+
+print("Moonlight shines bright on the temple walls")
